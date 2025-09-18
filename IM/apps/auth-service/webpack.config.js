706913da55f4f00ae363a6b2e0f5b1f0a3c91e09
infinite302im <infinite302im@gmail.com>
2025-09-18 14:53:56 +0100

    Update webpack.config.js

--- IM/apps/auth-service/webpack.config.js
+++ IM/apps/auth-service/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, 'dist'),
+    path: join(__dirname, 'out-tsc/auth-service'),
   },
   plugins: [
     new NxAppWebpackPlugin({

683b05401a70ea439ba46087be54ec07c3fc2193
infinite302im <infinite302im@gmail.com>
2025-09-20 08:13:15 +0100

    Delete IM directory

--- IM/apps/auth-service/src/routes/auth.router.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import { userRegistration } from "../controller/auth.controller";
-
-const router:Router = express.Router();
-
-
-router.post("/user-registration", userRegistration);
-
-export default router;
